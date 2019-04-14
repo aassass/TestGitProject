16e07c20d711605f02aa08e59c6a8868b2a4f791
aassass <aassas@bfigroupe.com>
2019-04-14 23:11:22 +0100

    First commit

---
+++ ProjectTest/src/TestBonjour.java
@@ -0,0 +1,10 @@
+
+public class TestBonjour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bonjour BFIii");
+		System.out.println("Bonjour tout le mondeeeee ");
+	}
+
+}
